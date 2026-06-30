65e7216d2d34cb2c52b8ac9c45fba9d86f2b0b11
Daniel235-web <samueledaniel235@gmail.com>
2026-06-30 12:07:31 +0100

    feat: implement file system state, enable multi-window concurrent support, and add screen locking functionality

--- redux/reducers.ts
+++ redux/reducers.ts
@@ -3,11 +3,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import statusSlice from '@/redux/features/status-slice';
 import backgroundImage from '@/redux/features/background-image-slice';
 import allApps from '@/redux/features/all-apps-slice';
+import fileSystemReducer from '@/redux/features/file-system-slice';
 
 const rootReducer = combineReducers({
   status: statusSlice,
   backgroundImage: backgroundImage,
   allApps: allApps,
+  fileSystem: fileSystemReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
