ed1f2f93f0571d53ee06338c2b2be1020800b87b
Daniel235-web <samueledaniel235@gmail.com>
2026-06-30 12:09:42 +0100

    feat: add isLocked state and lockScreen/unlockScreen actions to status slice

--- redux/features/status-slice.ts
+++ redux/features/status-slice.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface StatusState {
   soundLevel: number;
   brightnessLevel: number;
+  isLocked: boolean;
 }
 
 const initialState = {
   soundLevel: 50,
   brightnessLevel: 100,
+  isLocked: true,
 } as StatusState;
 
 export const status = createSlice({
@@ -21,8 +23,17 @@ export const status = createSlice({
     setBrightnessLevel: (state, action: PayloadAction<number>) => {
       state.brightnessLevel = action.payload;
     },
+
+    lockScreen: (state) => {
+      state.isLocked = true;
+    },
+
+    unlockScreen: (state) => {
+      state.isLocked = false;
+    },
   },
 });
 
-export const { setBrightnessLevel, setSoundLevel } = status.actions;
+export const { setBrightnessLevel, setSoundLevel, lockScreen, unlockScreen } =
+  status.actions;
 export default status.reducer;
