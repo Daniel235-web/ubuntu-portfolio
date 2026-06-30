68ebe1154057a5ee7021243604c9442ea5881d8e
Daniel235-web <samueledaniel235@gmail.com>
2026-06-30 14:40:25 +0100

    feat: implement apps drawer state management and sidebar toggle functionality

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,14 +1,18 @@
+import cls from 'classnames';
 import { Grid } from '@/components/icons';
 import { SidebarMenuItem } from '@/components/sidebar-menu-item';
 import { Tooltip } from '@/components/tooltip';
-import { useAppSelector } from '@/redux/hooks';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { toggleAppsDrawer } from '@/redux/features/status-slice';
 
 interface SidebarProps {}
 
 const Sidebar = ({}: SidebarProps) => {
+  const dispatch = useAppDispatch();
   const favoriteApps = useAppSelector((state) =>
     state.allApps.filter((app) => app.isFavorite || app.isOpen),
   );
+  const showAppsDrawer = useAppSelector((state) => state.status.showAppsDrawer);
 
   return (
     <div className="absolute left-0 top-0 z-40 flex h-full w-auto transform select-none flex-col items-center justify-start border-black border-opacity-30 bg-black bg-opacity-20 pt-7 duration-300">
@@ -23,7 +27,13 @@ const Sidebar = ({}: SidebarProps) => {
         </Tooltip>
       ))}
       <div
-        className={`m-1 flex h-10 w-10 items-center justify-center rounded hover:bg-white hover:bg-opacity-10`}
+        onClick={() => dispatch(toggleAppsDrawer())}
+        className={cls(
+          'm-1 flex h-10 w-10 cursor-pointer items-center justify-center rounded transition',
+          showAppsDrawer
+            ? 'bg-white bg-opacity-25 text-white'
+            : 'text-zinc-300 hover:bg-white hover:bg-opacity-10 hover:text-white',
+        )}
         style={{ marginTop: 'auto' }}
       >
         <Tooltip position="top" text="Show Applications">
